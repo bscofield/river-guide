49b2a3fd93c475dc5cc6b8d6e5b9cc3a4d4d89c6
Ben Scofield <git@turrean.com>
2016-08-07 08:38:00 -0400

    Fix logic error

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,13 +24,14 @@ class Scraper
   def self.scrape(ids)
     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
 
-    ids.map do |id|
+    puts "Beginning scrape..."
+    ids.shuffle.map do |id|
+      sleep rand(15)+15
       puts "Retrieving list #{id}"
       doc  = Nokogiri::HTML(open(URL.sub(/_ID_/, id), 'User-Agent' => user_agent))
       rows = doc.css('table.g-print-items tr')
       rows.shift
       rows.map { |r| ArBook.load(Book.new(r)) }
-      sleep rand(15)+15
     end.flatten
   end
 end
